07f6e365c1068c5f40b60428a9953e641a27a709
Pdtcode <trinhpeter15@gmail.com>
2022-08-22 22:13:24 -0500

    Initial commit

---
+++ app/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Peter Trinh wants to tell you something...</h1>
+        <box className="box">
+        <a href='https://youtube.com/watch?v=xvFZjo5PgG0' className="btn" >Click Me!</a>
+        </box>
+      </header>
+    </div>
+  );
+}
+
+export default App;
